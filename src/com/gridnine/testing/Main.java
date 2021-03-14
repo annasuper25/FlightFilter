788e0bcc7c2f17eee175ba9c6e78dac9f65a3616
Anna Efremova <19151892876@mail.ru>
2021-03-15 01:48:59 +0300

    third rule is implemented

--- src/com/gridnine/testing/Main.java
+++ src/com/gridnine/testing/Main.java
@@ -3,6 +3,7 @@ package com.gridnine.testing;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
@@ -30,7 +31,7 @@ public class Main {
 
         flights.forEach(System.out::println);
 
-        System.out.println(" ------ Исключили перелеты где имеются сегменты с датой прилёта раньше даты вылета: ------- ");
+        System.out.println(" ------ Исключили перелеты, где имеются сегменты с датой прилёта раньше даты вылета: ------- ");
 
         flights = flights.stream().filter(
                 flight -> flight.getSegments().stream()
@@ -38,8 +39,26 @@ public class Main {
 
         flights.forEach(System.out::println);
 
-        //System.out.println(" ****** Исключили перелеты, у которых общее время, проведённое на земле превышает два часа: ******* ");
-        //Здесь скоро будет код.
+        System.out.println(" ------ Исключили перелеты, у которых общее время, проведённое на земле, превышает два часа: ------- ");
 
+        Iterator<Flight> itr = flights.iterator();
+
+        while (itr.hasNext()) {
+            Flight flight = itr.next();
+            List<Segment> segments = flight.getSegments();
+
+                if (segments.size() > 1) {
+                    long sumTime = 0;
+                    for (int k = 0; k < segments.size() - 1; k++) {
+                        if (segments.get(k).getArrivalDate().isBefore(segments.get(k + 1).getDepartureDate())) {
+                            sumTime += Duration.between(segments.get(k).getArrivalDate(), segments.get(k + 1).getDepartureDate()).toMinutes();
+                            if (sumTime > 120) {
+                                itr.remove();
+                            }
+                        }
+                    }
+                }
+        }
+        flights.forEach(System.out::println);
     }
 }
